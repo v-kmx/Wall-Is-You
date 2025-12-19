92fadb75f5ed7ad1870cf424946fefa1b63aba21
noameezy <noam.driaqn@gmail.com>
2025-12-19 16:28:22 +0100

    Update wall_is_you_graphic.py

--- wall_is_you_graphic.py
+++ wall_is_you_graphic.py
@@ -103,7 +103,6 @@ def dessiner_case(carte, taille_case, aventurier):
                     x_centre = x_init + taille_case // 2
                     y_centre = y_init + taille_case // 2
                     image(x_centre, y_centre, "ressources/img/tresor.png", 32, 30)
-
             # Dessin de l'Aventurier
             if (r, c) == aventurier[0]:
                 lvl = aventurier[1]
@@ -260,12 +259,10 @@ def main(chargement_carte=0):
             intentions = modification_dessin(carte, taille_case, aventurier, intentions, longueur_tableau, hauteur_tableau)
         elif type_ev(evenement) == "ClicDroit":
             intentions = modifier_intention(carte, taille_case, aventurier, intentions, longueur_tableau, hauteur_tableau)
-        elif type_ev(evenement) == "ClicDroit":
-            c=carte[:]
-            y= ordonnee_souris() // taille_case
-            x= abscisse_souris() // taille_case
-            c[x][y][1]=-1
-            modification_dessin(c, taille_case, aventurier, intentions, longueur_tableau, hauteur_tableau)
+            x= ordonnee_souris() // taille_case
+            y= abscisse_souris() // taille_case
+            carte[x][y][1]=-1
+            modification_dessin(carte, taille_case, aventurier, intentions, longueur_tableau, hauteur_tableau)
         
         elif touche(evenement) == "space":
              while len(intentions) > 1 and not game_over:
@@ -301,4 +298,5 @@ def main(chargement_carte=0):
 
 
 # main()  # /!\ DEBUGGAGE // Lance le jeu graphique 
+
 
