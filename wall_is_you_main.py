c49a8f897dfb6418c04d215e7251e9ca40e5436f
eliepennetiermsika-bit <elie.pennetier.msika@gmail.com>
2026-01-09 17:18:57 +0100

    Update wall_is_you_main.py

--- wall_is_you_main.py
+++ wall_is_you_main.py
@@ -1,6 +1,7 @@
 #Projet wall_is_you SAE 1.0.1
 #groupe: Pennetier msika Elie, Kitadi Vianney, Diouri Noam
-from random import randint, random
+from random import randint
+from random import random
 
 def init_case(coordonnee,carte):
     """
@@ -16,6 +17,7 @@ def init_case(coordonnee,carte):
         sorties=[bool(randint(0,1)),bool(randint(0,1)),bool(randint(0,1)),bool(randint(0,1))]
     carte[i].append([sorties,None])
 
+
 def init_dragon(carte):
     """
     creer aleatoirement des dragons sur chaque case
@@ -27,12 +29,13 @@ def init_dragon(carte):
     """
     longueur_carte,hauteur_carte=len(carte),len(carte[0])
     niveau_dragons=1
-    probabilite_apparition=0.04
+    probabilite_apparition=0.07
     for i in range(longueur_carte):
         for j in range(hauteur_carte):
             if random()<=probabilite_apparition and carte[i][j][1]==None:
                 carte[i][j][1]=niveau_dragons
                 niveau_dragons+=1
+
 
 def init_aventurier(coordonnee=(0,0)):
     #posiblement obsolete, je vous laisse me le dire
@@ -44,63 +47,58 @@ def init_aventurier(coordonnee=(0,0)):
     """
     return [coordonnee,1]  
 
+
 def verifier_validite_intention(carte, liste_intention):
-    """
-    Vérifie la validité de la dernière case entrée dans la liste intention.
-    Renvoie un booléen.
-    """
-    i = len(liste_intention) - 1
-    Coord_suggeree = liste_intention[i]
-    Coord_precedente = liste_intention[i - 1]
+    if len(liste_intention) < 2:
+        return True  # rien à vérifier
 
-    a, b = Coord_suggeree
-    c, d = Coord_precedente
-    # Check si cases sont identiques
-    if (a, b) == (c, d):
+    (r2, c2) = liste_intention[-1]  # case suggérée
+    (r1, c1) = liste_intention[-2]  # case précédente
+
+    # mêmes coordonnées
+    if (r1, c1) == (r2, c2):
         return False
 
-    # Check abscisse: doivent être adjacents horizontalement
-    if abs(a - c) > 1:
-        return False
+    # vérifier l'adjacence
+    dr = r2 - r1
+    dc = c2 - c1
+    if abs(dr) + abs(dc) != 1:
+        return False  # pas adjacente
 
-    # Check ordonnée: doivent être adjacents verticalement
-    if abs(b - d) > 1:
-        return False
+    sorties_precedente = carte[r1][c1][0]
+    sorties_suggeree = carte[r2][c2][0]
 
-    # Comparaison sorties
-    # Adjacent verticalement
-    if a == c:
-        # vers le haut
-        if b > d and carte[a][b][0][3] and carte[c][d][0][1]:
-            return True
-        # vers le bas
-        if b < d and carte[a][b][0][1] and carte[c][d][0][3]:
-            return True
+    # mouvement vertical
+    if dr == -1:  # vers le haut
+        return sorties_precedente[1] and sorties_suggeree[2]
+    if dr == 1:   # vers le bas
+        return sorties_precedente[2] and sorties_suggeree[1]
 
-    # Adjacent horizontalement
-    if b == d:
-        # vers la gauche
-        if a > c and carte[a][b][0][0] and carte[c][d][0][2]:
-            return True
-        # vers la droite
-        if a < c and carte[a][b][0][2] and carte[c][d][0][0]:
-            return True
+    # mouvement horizontal
+    if dc == -1:  # vers la gauche
+        return sorties_precedente[0] and sorties_suggeree[3]
+    if dc == 1:   # vers la droite
+        return sorties_precedente[3] and sorties_suggeree[0]
 
     return False
+
+
+
 
 def ajouter_intention(coordonnee_clic,carte,liste_intention):
     """
     verifie si c'est possible et ajoute une intention à l'aventurier
     (dans la beta, celle ci sera définis manuellement)
     """
-    if coordonnee_clic in liste_intention:
+    if coordonnee_clic in liste_intention or carte[coordonnee_clic[0]][coordonnee_clic[1]]==None:
         supprimer_intention(coordonnee_clic,liste_intention)
         return
     liste_intention.append(coordonnee_clic)
     if not verifier_validite_intention(carte,liste_intention):
         liste_intention.pop()
     return
-        
+  
+  
 def supprimer_intention(coordonnee_clic,liste_intention):
     """
     supprime la dernière intention si le joueur clique sur une case
@@ -108,6 +106,7 @@ def supprimer_intention(coordonnee_clic,liste_intention):
     """
     if not(len(liste_intention)==1) and coordonnee_clic in liste_intention:
         liste_intention.pop()
+
 
 def verif_intention_global(aventurier,carte,liste_intention):
     """
@@ -127,104 +126,38 @@ def verif_intention_global(aventurier,carte,liste_intention):
         nouvelle_liste_intention.append(liste_intention[i])
     return nouvelle_liste_intention
 
-def modifier_case(coordonnee, carte):
-    """
-    tourne une case de 90° : Haut devient Droite, Droite devient Bas, etc.
-    """
-    i, j = coordonnee
-    h, d, b, g = carte[i][j][0]
-    carte[i][j][0] = [g, h, d, b]
-def deplacer_dragons(carte):
-    """
-    Déplace les dragons en respectant strictement le dessin des murs :
-    sorties[0]=Haut, [1]=Droite, [2]=Bas, [3]=Gauche
-    """
-    longueur = len(carte)
-    hauteur = len(carte[0])
-    mouvements = []
-    
-    for i in range(longueur):
-        for j in range(hauteur):
-            niveau = carte[i][j][1]
-            if niveau is None or niveau < 1:
-                continue
-            
-            sorties_ici = carte[i][j][0]
-            directions_valides = []
 
-            # TENTATIVE HAUT : ligne i-1
-            # Sortie HAUT (0) ici ET Entrée BAS (2) chez le voisin
-            if i > 0 and sorties_ici[0] and carte[i-1][j][0][2] and carte[i-1][j][1] is None:
-                directions_valides.append((i-1, j))
-            
-            # TENTATIVE DROITE : colonne j+1
-            # Sortie DROITE (1) ici ET Entrée GAUCHE (3) chez le voisin
-            if j < hauteur - 1 and sorties_ici[1] and carte[i][j+1][0][3] and carte[i][j+1][1] is None:
-                directions_valides.append((i, j+1))
-                
-            # TENTATIVE BAS : ligne i+1
-            # Sortie BAS (2) ici ET Entrée HAUT (0) chez le voisin
-            if i < longueur - 1 and sorties_ici[2] and carte[i+1][j][0][0] and carte[i+1][j][1] is None:
-                directions_valides.append((i+1, j))
-
-            # TENTATIVE GAUCHE : colonne j-1
-            # Sortie GAUCHE (3) ici ET Entrée DROITE (1) chez le voisin
-            if j > 0 and sorties_ici[3] and carte[i][j-1][0][1] and carte[i][j-1][1] is None:
-                directions_valides.append((i, j-1))
-
-            if directions_valides:
-                nx, ny = directions_valides[randint(0, len(directions_valides) - 1)]
-                mouvements.append(((i, j), (nx, ny), niveau))
-
-    for (old_i, old_j), (new_i, new_j), niveau in mouvements:
-        if carte[new_i][new_j][1] is None:
-            carte[old_i][old_j][1] = None
-            carte[new_i][new_j][1] = niveau
+def modifier_case(coordonnee,carte):
+    """
+    tourne une case de 90° (modifie le tuple de booléén pour que cela corresponde)
+    """
+    i,j=coordonnee
+    gauche,haut,bas,droite=carte[i][j][0]
+    carte[i][j][0]=[bas,gauche,droite,haut]
+ 
+ 
 def tour_jeu(carte,intention,aventurier):
     """
     fait avancer l'enventurier d'une case selon son intention
     et combattre si ce dernier termine son deplacement sur une case contenant
     un dragon
     renvoi un booléen définissant ou non la fin du jeu
-    // update rendu2
-    désormais, tour jeu gère maintenant le déplacement de l'aventurier et des dragons
-    (fonctionne avec deplacer_dragons)
     """
     if len(intention)>1:
         aventurier[0]=intention.pop(1)
     position_x_aventurier,position_y_aventurier=aventurier[0]
     intention[0]=(position_x_aventurier,position_y_aventurier)
-    
-    objet_case = carte[position_x_aventurier][position_y_aventurier][1]
-    if objet_case is not None:
-        # Cas d'un Dragon (Combat)
-        if objet_case >= 1:
-            if objet_case <= aventurier[1]:
-                if objet_case > 50:
-                    aventurier[1] += objet_case
-                else:
-                    carte[position_x_aventurier][position_y_aventurier][1] = None
-                    aventurier[1] += 1
-                return False
+    if carte[position_x_aventurier][position_y_aventurier][1]!=None:
+        if carte[position_x_aventurier][position_y_aventurier][1]<=aventurier[1]:
+            if carte[position_x_aventurier][position_y_aventurier][1]>50:
+                aventurier[1]+=carte[position_x_aventurier][position_y_aventurier][1]
             else:
-                return True
-        # Cas d'un Trésor (Récupération)
-        elif objet_case < 0:
-            aventurier[1] += abs(objet_case)
-            carte[position_x_aventurier][position_y_aventurier][1] = None
+                carte[position_x_aventurier][position_y_aventurier][1]=None
+                aventurier[1]+=1
+            return False
+        else:
+            return True
 
-    # Fin du tour de l'aventurier
-    if len(intention) == 1:
-        deplacer_dragons(carte)
-        objet_final = carte[position_x_aventurier][position_y_aventurier][1]
-        if objet_final is not None and objet_final >= 1:
-            if objet_final <= aventurier[1]:
-                 carte[position_x_aventurier][position_y_aventurier][1] = None
-                 aventurier[1] += 1
-            else:
-                 return True
-
-    return False
 
 def init_carte(longueur_carte,hauteur_carte):
     """
@@ -239,6 +172,7 @@ def init_carte(longueur_carte,hauteur_carte):
         for j in range(hauteur_carte):
             init_case((i,j),carte)
     return carte
+
 
 def test_objets(carte):
     """
@@ -258,9 +192,12 @@ def test_objets(carte):
                 compte_tresor.append((i,j,carte[i][j][1]))
     return compte_dragon,compte_tresor
 
+
 def encode_case(carte):
     """
     encode les informations des cases de la carte et les renvois
+    (l'encodage correspond au symbole boite)
+    (ne prend pas en compte les dragons le hero ou les objets)
     """
     traduction_box2={(True,False,False,False):u'\u2561',(False,True,False,False):u'\u2568',(False,False,True,False):u'\u2565',(False,False,False,True):u'\u255E',(True,False,False,True):u'\u2550',(False,True,True,False):u'\u2551',(False,False,True,True):u'\u2554',(True,False,True,False):u'\u2557',(False,True,False,True):u'\u255A',(True,True,False,False):u'\u255D',(False,True,True,True):u'\u2560',(True,True,True,False):u'\u2563',(True,False,True,True):u'\u2566',(True,True,False,True):u'\u2569',(True,True,True,True):u'\u256C'}
     carte_encode=list()
@@ -270,9 +207,23 @@ def encode_case(carte):
             carte_encode[i].append(traduction_box2[tuple(carte[i][j][0])])
     return carte_encode
 
-def sauvegarde(carte,intention,aventurier):
+
+def recuperer_ligne(carte,indice_ligne):
+    resultat=list()
+    for i_colonne in range(len(carte)):
+        resultat.append(carte[i_colonne][indice_ligne])
+    return resultat
+
+
+def sauvegarde(carte,intention,aventurier,nb_tresor):
     """
     sauvegarde l'état de la carte actuelle dans un fichier text
+    appelé "sauvegarde_derniere_partie.txt
+    l'état comprend:
+    la position et niveau du héro
+    les informations des cases (position,dragon,sorties)
+    la taille de la carte
+    l'intention
     """
     fichier_sauvegarde=open("save/sauvegarde_dernier_donjon","w")
     carte_encode=encode_case(carte)
@@ -296,32 +247,42 @@ def sauvegarde(carte,intention,aventurier):
         if type(tresor)==int:
             continue
         fichier_sauvegarde.write("T "+str(tresor[0])+" "+str(tresor[1])+" "+str(tresor[2])+'\n')
-    fichier_sauvegarde.write("\n")
-
+    fichier_sauvegarde.write(str(nb_tresor))
+    
+    
 def decode_cases(case_encode):
     """
     decodes les informations des cases de la carte et les renvois
+    (le decodage correspondant au symbole boite traduit en sortie)
+    (ne prend pas en compte les dragons le hero ou les objets)
     """
     traduction_box2={u'\u2561':(True,False,False,False),u'\u2568':(False,True,False,False),u'\u2565':(False,False,True,False),u'\u255E':(False,False,False,True),u'\u2550':(True,False,False,True),u'\u2551':(False,True,True,False),u'\u2554':(False,False,True,True),u'\u2557':(True,False,True,False),u'\u255A':(False,True,False,True),u'\u255D':(True,True,False,False),u'\u2560':(False,True,True,True),u'\u2563':(True,True,True,False),u'\u2566':(True,False,True,True),u'\u2569':(True,True,False,True),u'\u256C':(True,True,True,True)}
     return traduction_box2[case_encode]
 
+
 def recuperer_int(chaine):
     """
     renvoie le premier chiffre qu'il trouve et l'indice ou il s'est arreter
+    (un chiffre est considerer comme une suite de numero et il renvoi
+    la premiere suite de numéro qu'il croise)
+    si il n'en croise aucune, il renvoi (None,None)
     """
     chiffre=""
     indice_caractere=0
+    repetition=0
     for i in range(len(chaine)):
         if chaine[indice_caractere].isdigit():
             while indice_caractere<len(chaine) and chaine[indice_caractere].isdigit():
                 chiffre+=chaine[indice_caractere]
                 indice_caractere+=1
+                repetition+=1
             if indice_caractere==len(chaine)-1:
                 return int(chiffre),indice_caractere
             return int(chiffre),indice_caractere+1
         else:
             indice_caractere+=1
     return None,None
+
 
 def recuperer_chiffre(chaine):
     """
@@ -338,56 +299,80 @@ def recuperer_chiffre(chaine):
         indice_traite+=indice_temp
     return resultat
 
+
 def chargement(niveau):
     """
     cette fonction permet de lire les informations contenue sur un fichier
+    et de les renvoyer sous une forme valide pour etre utilisé ailleurs
+    (pour l'instant uniquement du def main_handler():
+    # Crée la fenêtre une seule foisdernier donjon, à l'avenir aussi des
+    niveaux prédéfinis)
     """
     if niveau<0:
         fichier_chargement=open("save/sauvegarde_dernier_donjon","r")
     else:
-        fichier_chargement=open("niveau_donjon"+str(niveau),"r")
+        fichier_chargement=open("level/niveau"+str(niveau),"r")
     fichier_chargement=fichier_chargement.readlines() 
     carte=list()
-    for colonne in range(len(fichier_chargement[0])-1):
-        carte.append([])
     
+    indice_ligne=0
+    while fichier_chargement[indice_ligne] != "\n":
+        carte.append([])
+        indice_ligne+=1
+        
     indice_ligne=0
     aventurier=[(0,0),1]
     intention=list()
-    
+    nb_tresor=0
+
+    #recuperation information basique case (sorties)
     while fichier_chargement[indice_ligne]!="\n":
         for case in fichier_chargement[indice_ligne]:
             if not(case=="\n"):
                 carte[indice_ligne].append([decode_cases(case),None])
         indice_ligne+=1
     
+    #recuperation information aventurier
     indice_ligne+=1
+    assert fichier_chargement[indice_ligne][0]=="A","une erreur est survenue dans le chargement, vérifier le chargement ou debugger le programme"
     informations_aventurier=recuperer_chiffre(fichier_chargement[indice_ligne])
     aventurier[0]=(informations_aventurier[0],informations_aventurier[1])
     aventurier[1]=informations_aventurier[2]
     indice_ligne+=2
     
+    #recuperation de l'intention
     while fichier_chargement[indice_ligne][0]=="I":
         intention.append(tuple(recuperer_chiffre(fichier_chargement[indice_ligne])))
         indice_ligne+=1
     indice_ligne+=1
 
-    while indice_ligne < len(fichier_chargement) and fichier_chargement[indice_ligne][0] in ["D","T"]:
+    #recuperation des dragons
+    while fichier_chargement[indice_ligne][0] in ["D","T"]:
         information_objet=recuperer_chiffre(fichier_chargement[indice_ligne])
         carte[information_objet[0]][information_objet[1]][1]=information_objet[2]
         indice_ligne+=1
-    return carte,intention,aventurier
+    indice_ligne+=1
+    
+    nb_tresor=int(fichier_chargement[indice_ligne])
+    
+    return carte,intention,aventurier,nb_tresor
+
 
 def a_gagner(carte):
     """
     fonction vérifiant si le nombre de dragon est = à 0
+    renvoi un booléen
     """
     liste_information_dragon,tresors=test_objets(carte)
     return liste_information_dragon[0]==0
         
 def ee(carte,aventurier):
     """
-    Easter Egg
+    fonction la PLUS importante
+    elle gère toutes les positions
+    et vos finances
+    et vous fait de la choucroute
+    non...elle gère les Easter Egg
     """
     evenement=randint(0,2)
     x,y=aventurier[0]
@@ -412,8 +397,8 @@ def ee(carte,aventurier):
             for j in range(len(carte[0])):
                 position_porte=randint(0,3)
                 carte[i][j][0]=possibilite[position_porte]
-    return carte,aventurier 
-
+    return carte,aventurier
+      
 def taille_optimale(taille_x_max,taille_y_max,taille_x_tableau,taile_y_tableau):
     """
     renvoie la taille maximale pour afficher les cases entièrement dans l'écran
@@ -422,6 +407,19 @@ def taille_optimale(taille_x_max,taille_y_max,taille_x_tableau,taile_y_tableau):
     while taille*taille_x_tableau<taille_x_max and taille*taile_y_tableau<taille_y_max:
         taille+=1
     return taille
+
+def test_encode_case():
+    """
+    affiche les sorties disponible associé à chaque symbole
+    """
+    traduction_box2={(True,False,False,False):u'\u2561',(False,True,False,False):u'\u2568',(False,False,True,False):u'\u2565',(False,False,False,True):u'\u255E',(True,False,False,True):u'\u2550',(False,True,True,False):u'\u2551',(False,False,True,True):u'\u2554',(True,False,True,False):u'\u2557',(False,True,False,True):u'\u255A',(True,True,False,False):u'\u255D',(False,True,True,True):u'\u2560',(True,True,True,False):u'\u2563',(True,False,True,True):u'\u2566',(True,True,False,True):u'\u2569',(True,True,True,True):u'\u256C'}
+    sorties=["gauche","haut","bas","droite"]
+    for sortie_possible in traduction_box2:
+        chaine=""
+        for sortie in range(len(sortie_possible)):
+            if sortie_possible[sortie]:
+                chaine+=sorties[sortie]+" "
+        print(chaine,traduction_box2[sortie_possible])
 """
 def main():
     
